652e90d76b8866c27dba1580c646d2fd404d0209
DeepthiSarayuGunda <gund0028@algonquinlive.com>
2026-06-12 13:45:33 -0400

    Lab 3 - Workshop RSVP MVC application

---
+++ Models/Rsvp.cs
@@ -0,0 +1,9 @@
+namespace WorkshopRSVP.Models
+{
+    public class Rsvp
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool NeedsAccommodation { get; set; }
+        public string WorkshopTitle { get; set; } = string.Empty;
+    }
+}
